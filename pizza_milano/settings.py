5c34af3fa37be71245e8f3d908ec5ee25f4f4555
Nyberg988 <carl.nyberg@hotmail.com>
2022-10-25 15:59:57 +0000

    Removed secret key and set debug

--- pizza_milano/settings.py
+++ pizza_milano/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)f6fpfqjs97rx#2wd%d(_7!t)_(do8=(z8mx^801-6@51^^-#q'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['nyberg988-pizza-milano.herokuapp.com', 'localhost']
 
